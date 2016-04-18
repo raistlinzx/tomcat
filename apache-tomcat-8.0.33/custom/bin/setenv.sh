8f0731cc4f9a9df5f12c03ed1971aeda9fe986b1
赵欣 <zhaoxin@qiyestore.com>
2016-04-18 12:07:58 +0800

    增加jmx配置

--- apache-tomcat-8.0.33/custom/bin/setenv.sh
+++ apache-tomcat-8.0.33/custom/bin/setenv.sh
@@ -3,4 +3,4 @@
 # CATALINA_OPTS are used during START/RUN
 CATALINA_PID="$CATALINA_BASE/logs/tomcat.pid"
 JVM_OPTS="-Xmx256M -Xss256K"
-CATALINA_OPTS="$JVM_OPTS"
+CATALINA_OPTS="$JVM_OPTS -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote.rmi.port=6969 -Dcom.sun.management.jmxremote.port=6969 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false "
